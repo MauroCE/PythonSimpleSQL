36fdc516bf680702426fc47edfa4256306b150eb
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2018-07-26 17:50:32 +0200

    added username and password to config

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ SQL_DRIVER = ""  # For example 'SQL Server'
 SQL_DBO = ""     # For example 'dbo'
 SQL_DB = ""      # For example 'master'
 SQL_SERVER = ""  # For example '.\SQLEXPRESS'
+SQL_USER = ""
+SQL_PWD = ""
